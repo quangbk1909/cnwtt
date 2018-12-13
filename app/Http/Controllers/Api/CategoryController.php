380e800c991cda5162651e40b0f75747ef51eac4
Hieu Nguyen <minhhieun4@gmail.com>
2018-12-13 19:00:20 +0700

    add author for every post api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\User;
 use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -17,13 +18,49 @@ class CategoryController extends Controller
     public function getCategoryPostFromNewest($categoryID){
         $category = Category::find($categoryID);        
         $categoryPost = $category->posts()->orderBy('created_at','DESC')->get();
-        return response() -> json($categoryPost);
+        $responseArray = array();
+        foreach ($categoryPost as $post){
+            $userOfPost = User::where('id',$post->user_id)->first();
+
+            $arrayPost = array();
+            $arrayPost["post_id"] = $post->id;
+            $arrayPost["title"] = $post->title;
+            $arrayPost["content"] = $post->content;
+            $arrayPost["date_created"] = $post->created_at;
+            $arrayPost["vote"] =$post->vote_numbers;
+            $arrayPost["image_post"] = $post->image_name;
+            $arrayPost["author_id"] = $userOfPost->id;
+            $arrayPost["author_name"] = $userOfPost->name;
+            $arrayPost["avatar"] = $userOfPost->image_link;
+
+            array_push($responseArray,$arrayPost);
+        }
+        // json_encode($responseArray);
+        return response() -> json($responseArray);
     }
 
     public function getCategoryPostPopular($categoryID){
         $category = Category::find($categoryID);
         $categoryPost = $category->posts()->orderBy('vote_numbers','DESC')->get();
-        return response() -> json($categoryPost);
+        $responseArray = array();
+        foreach ($categoryPost as $post){
+            $userOfPost = User::where('id',$post->user_id)->first();
+
+            $arrayPost = array();
+            $arrayPost["post_id"] = $post->id;
+            $arrayPost["title"] = $post->title;
+            $arrayPost["content"] = $post->content;
+            $arrayPost["date_created"] = $post->created_at;
+            $arrayPost["vote"] =$post->vote_numbers;
+            $arrayPost["image_post"] = $post->image_name;
+            $arrayPost["author_id"] = $userOfPost->id;
+            $arrayPost["author_name"] = $userOfPost->name;
+            $arrayPost["avatar"] = $userOfPost->image_link;
+
+            array_push($responseArray,$arrayPost);
+        }
+        // json_encode($responseArray);
+        return response() -> json($responseArray);
     }
 
     public function getDescription($categoryID){
@@ -32,7 +69,7 @@ class CategoryController extends Controller
             [
                 'id'=>$category->id,
                 'name'=>$category->name,
-                // 'description'=>$category->description
+                'description'=>$category->description
             ]
             );
     }
